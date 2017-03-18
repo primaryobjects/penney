f89aa657ecf908270adab483f71230735dfa77b8
Kory Becker <kbecker@primaryobjects.com>
2017-03-17 22:28:56 -0400

    Initial commit.

---
+++ penny.R
@@ -0,0 +1,97 @@
+packages <- c('ggplot2', 'maps', 'ggmap')
+if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
+  install.packages(setdiff(packages, rownames(installed.packages())))  
+}
+
+if (!require("ghit")) {
+  install.packages("ghit")
+  # on 64-bit Windows
+  ghit::install_github(c("ropensci/tabulizerjars", "ropensci/tabulizer"), INSTALL_opts = "--no-multiarch")
+  # elsewhere
+  #ghit::install_github(c("ropensci/tabulizerjars", "ropensci/tabulizer"))
+}
+
+library(tabulizer)
+library(ggplot2)
+library(maps)
+library(ggmap)
+
+# Read PDF and extract data.
+url <- 'http://www.jcpnewsroom.com/news-releases/2017/assets/0317_list_of_store_closures.pdf'
+data <- extract_tables(url, guess = F, area = list(c(300, 0, 800, 800), c(0, 0, 800, 800), c(0, 0, 800, 800), c(0, 0, 800, 800)))
+
+# Remove missing column and skip first row (table header).
+data[[1]] <- data[[1]][-1, c(1,2,4)]
+
+# Combine pages into a single set and cast to data.frame.
+data <- as.data.frame(do.call(rbind, data[-length(data)]))
+
+# Set column names.
+names(data) <- c('Mall', 'City', 'State')
+
+states <- map_data('state')
+counties <- map_data('county')
+
+# Get lat/long coordinates for each location.
+set <- paste(data$City, data$State, sep=' ')
+data <- cbind(data, geocode(set))
+data$group=0
+
+# Get counts of closures per state (for coloring on map).
+countPerState <- as.data.frame(table(data$State))
+names(countPerState) <- c('abb', 'freq')
+
+# Get state map.
+US <- map_data("state")
+
+# Get state abbreviations.
+US$abb <- sapply(US$region, function(region) { 
+  c(state.abb[region == tolower(state.name)])
+})
+US$abb <- as.factor(as.character(US$abb))
+
+# Add counts per state to map.
+US$count <- as.numeric(sapply(US$abb, function(abb) {
+  countPerState[countPerState$abb == as.character(abb), 'freq']
+}))
+
+# For any states with no data, set their count to 0.
+US$count[is.na(US$count)] <- 0
+
+# Plot US map simple, no alpha fill, no labels.
+p <- ggplot(US, aes( x = long , y = lat, group=group) ) +
+  geom_polygon( colour = "green", fill= 'lightgreen' ) +
+  expand_limits( x = US$long, y = US$lat ) +
+  coord_fixed() +
+  geom_path( data = states , colour = "white")
+
+# Hide x and y axis tick mark labels.
+p <- p + theme(
+  axis.text.x = element_blank(),
+  axis.text.y = element_blank(),
+  axis.ticks = element_blank())
+
+# Plot points.
+p <- p + geom_point(data=data, aes(x=lon, y=lat), colour="red", fill="Pink",pch=21, size=3, alpha=I(0.7))
+p <- p + xlab('JCPenny Store Closings') + ylab('')
+p
+
+
+# Plot US map detailed.
+p <- ggplot(US, aes(x = long, y = lat, group=group)) +
+  geom_polygon(fill='lightgreen', aes(alpha=count)) +
+  geom_path() +
+  coord_fixed() +
+  guides(alpha=FALSE)
+
+# Hide x and y axis tick mark labels.
+p <- p + theme(
+  axis.text.x = element_blank(),
+  axis.text.y = element_blank(),
+  axis.ticks = element_blank())
+
+# Plot points.
+p <- p + geom_point(data=data, aes(x=lon, y=lat), colour="red", fill="pink", pch=21, size=3, alpha=I(0.7))
+p <- p + geom_text(data=data, aes(x=lon, y=lat, label=Mall), size=2.2, colour="black", hjust=-0.09, vjust=0, alpha=I(1))
+p <- p + xlab('JCPenny Store Closings') + ylab('')
+p
---
+++ readme.md
@@ -0,0 +1,39 @@
+JCPenny Store Closings - March 2017
+-------------------------------
+
+A simple data analysis and plot of all JCPenny store closings for Match, 2017.
+
+This project reads the official JCPenny store closing PDF file, extracts the list of store locations, and plots the results on a US state map.
+
+## The Dataset
+
+Data was collected from the official JCPenny store closing [PDF](http://www.jcpnewsroom.com/news-releases/2017/assets/0317_list_of_store_closures.pdf) file. A local copy is also [available](https://raw.githubusercontent.com/primaryobjects/penny/master/data/store_closures.pdf).
+
+The dataset contains the following fields:
+
+Mall/Shopping Center
+City
+State
+
+Geo-location coordinates for latitude and longitude are retrieved from a Google geo-coding API, based upon the store City and State. The number of store closures per state are shaded on the US map, with darker colors representing more closures within that state.
+
+## Results
+
+![March 2017 JCPenny Store Closures - Overview](https://raw.githubusercontent.com/primaryobjects/pennny/master/images/jcpenny.png)
+
+![March 2017 JCPenny Store Closures - Detailed](https://raw.githubusercontent.com/primaryobjects/pennny/master/images/jcpenny-detail.png)
+
+## References
+
+[Yahoo Finance](http://finance.yahoo.com/news/138-jc-penney-stores-close-130436124.html)
+
+[JCP Newsroom](http://www.jcpnewsroom.com/news-releases/2017/assets/0317_list_of_store_closures.pdf)
+
+## Copyright
+
+Copyright (c) 2017 Kory Becker http://primaryobjects.com/kory-becker
+
+## Author
+
+Kory Becker
+http://www.primaryobjects.com
